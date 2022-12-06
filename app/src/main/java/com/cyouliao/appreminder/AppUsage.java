a2417dcd1a21b7703eb6662e3676b37b56abfdf6
guoyang33 <guoyang33@gmail.com>
2022-12-06 13:50:47 +0800

    update 2022-12-06

--- app/src/main/java/com/cyouliao/appreminder/AppUsage.java
+++ app/src/main/java/com/cyouliao/appreminder/AppUsage.java
@@ -13,12 +13,10 @@ import java.util.List;
 public class AppUsage {
 
     private UsageStatsManager usageStatsManager;
-//    private AppCategory appCategory;
 
     public AppUsage(Context context, UsageStatsManager usageStatsManager) {
 
         this.usageStatsManager = usageStatsManager;
-//        appCategory = new AppCategory(context);
 
     }
 
@@ -34,34 +32,15 @@ public class AppUsage {
             startDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
             endDate = new Date(startDate.getTime() + (24 * 60 * 60 * 1000));
             usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startDate.getTime(), endDate.getTime());
-//            Map<String, String> appCategoryMap = appCategory.getAppCategoryMap();
-//            ArrayList<String> newPackageNameArray = new ArrayList<>();
             for (UsageStats usageStats :usageStatsList) {
                 // 使用時間 api 29以前不能用getTotalTimeVisible()
                 Long usageTime = null;
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     usageTime = (usageStats.getTotalTimeVisible()-usageStats.getTotalTimeVisible()%1000)/1000;
-//                    System.out.println("total time visiblel: " + (usageStats.getTotalTimeVisible()-usageStats.getTotalTimeVisible()%1000)/1000);
                 } else {
                     usageTime = (usageStats.getTotalTimeInForeground()-usageStats.getTotalTimeInForeground()%1000)/1000;
-//                    System.out.println("total time visiblel: " + (usageStats.getTotalTimeInForeground()-usageStats.getTotalTimeInForeground()%1000)/1000);
                 }
-//                if (usageTime != null) {
-//                    String packageName = usageStats.getPackageName();
-//                    if (!appCategoryMap.containsKey(packageName)) {
-//                        newPackageNameArray.add(packageName);
-//                    } else {
-//                        String category = appCategoryMap.get(packageName);
-//                        System.out.println("package name: " + usageStats.getPackageName());
-//                        System.out.println("app category: " + category);
-//                        System.out.println("usage time: " + usageTime);
-//                    }
-//                }
             }
-//            System.out.println("new package name array size: " + newPackageNameArray.size());
-//            if (newPackageNameArray.size() > 0) {
-//                appCategory.fetchRemote(newPackageNameArray);
-//            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
